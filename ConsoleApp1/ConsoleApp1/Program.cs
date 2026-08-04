bbec0229756ae1ac6b09c0ff21f6b470024bef82
kundiramashia9-web <kundiramashia9@gmail.com>
2026-08-04 14:32:44 +0200

    Create Program.cs
    I created program .cs

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,10 @@
+﻿using System;
+namespace Example
+{     class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
