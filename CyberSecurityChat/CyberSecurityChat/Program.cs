ace7c021e745960a49e3661b35cae8c02001b235
kundiramashia9-web <kundiramashia9@gmail.com>
2026-08-24 15:08:54 +0200

    Create Program.cs

---
+++ CyberSecurityChat/CyberSecurityChat/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using CyberSecurityChat.Services;
+
+namespace CyberSecurityChat
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Title = "🛡️ Advanced Cybersecurity Bot";
+            try { Console.SetWindowSize(140, 50); } catch { }
+
+            var uiService = new UIService();
+            var chatService = new ChatService();
+
+            uiService.DisplayAnimatedLogo();
+            uiService.DisplayTextGreeting();
+
+            var userName = chatService.GetUserName();
+            chatService.StartChat(userName);
+            uiService.DisplayFarewell(userName);
+        }
+    }
+}
