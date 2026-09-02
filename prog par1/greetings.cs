775979edf94b6cc9b12ca0d3662f2bf8ac2e6e1c
kundiramashia9-web <kundiramashia9@gmail.com>
2026-09-02 15:11:16 +0200

    h

---
+++ prog par1/greetings.cs
@@ -0,0 +1,28 @@
+﻿using System.Media;
+
+namespace ChatbotBot
+{
+    public class Greetings
+    {
+        public static void GreetUser(string userName)
+        {
+            Console.WriteLine($"Hello, {userName}! Welcome to the Chatbot.");
+            PlayGreetingSound();
+        }
+        private static void PlayGreetingSound()
+        {
+            try
+            {
+                using (SoundPlayer player = new SoundPlayer("WhatsApp Ptt 2026-08-19 at 12.43.50.wav"))
+                {
+                    player.PlaySync();
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Error playing sound: {ex.Message}");
+            }
+        }
+    }
+}
+
